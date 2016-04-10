584c9ead15bc96ab16cf9a2b457790213d6f71a7
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2016-04-09 21:51:50 -0700

    Update version to 0.1.1

--- lib/folder_template/version.rb
+++ lib/folder_template/version.rb
@@ -1,12 +1,12 @@
 # =============================================================================
-#  
+#
 # MODULE      : lib/folder_template/version.rb
 # PROJECT     : FolderTemplate
-# DESCRIPTION : 
+# DESCRIPTION :
 #
 # Copyright (c) 2016, Marc-Antoine Argenton.  All rights reserved.
 # =============================================================================
 
 module FolderTemplate
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
