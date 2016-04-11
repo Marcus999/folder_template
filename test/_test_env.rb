708f561e6e3119f05fcad9fd64d4fc7910fe97ab
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2016-04-11 00:26:42 -0700

    Fix ruby 2.2 minitest dependency and add minitest-reporters

--- test/_test_env.rb
+++ test/_test_env.rb
@@ -1,19 +1,21 @@
 # =============================================================================
-#  
+#
 # MODULE      : test/_test_env.rb
 # PROJECT     : RubyProjectGenerator
-# DESCRIPTION : 
+# DESCRIPTION :
 #
 # Copyright (c) 2016, Marc-Antoine Argenton.  All rights reserved.
 # =============================================================================
 
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'fileutils'
 require 'pp'
 require 'rr'
 
 require 'lib/folder_template.rb'
 
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
 
 module Minitest::Assertions
@@ -23,7 +25,7 @@ module Minitest::Assertions
       "Expected folders content to match:\n#{diffs}\n"
       # "Expected folders to match:\nexpected: #{expected_folder.inspect}\nactual: #{expected_folder.inspect}\ndiffs:\n#{diffs}"
   end
-  
+
 private
   def _diff_folders( expected_folder, actual_folder )
     `diff -r -u --exclude=.DS_Store #{expected_folder} #{actual_folder}`
--- test/test_fs_adapter.rb
+++ test/test_fs_adapter.rb
@@ -1,8 +1,8 @@
 # =============================================================================
-#  
+#
 # MODULE      : lib/ruby_project_generator/test_fs_adapter.rb
 # PROJECT     : RubyProjectGenerator
-# DESCRIPTION : 
+# DESCRIPTION :
 #
 # Copyright (c) 2016, Marc-Antoine Argenton.  All rights reserved.
 # =============================================================================
@@ -16,41 +16,39 @@ module FolderTemplate
   describe FsAdapter do
     let( :tmpdir ) { "tmp" }
     subject { FsAdapter.new( tmpdir ) }
-  
+
     before do
       stub(FileUtils).makedirs()
       stub(File).open()
       stub(File).write()
     end
-  
+
     describe "when calling makedirs()" do
       it "expands filename and calls FileUtils.makedirs()" do
         mock(FileUtils).makedirs( File.join( tmpdir, "aaa/bbb" ) )
         subject.makedirs( "aaa/bbb" )
       end
     end
-    
+
     describe "when calling write_to_file()" do
       it "expands filename, creates containing folder and calls File.write()" do
         expected_filename = File.join( tmpdir, "aaa/bbb" )
         mock(FileUtils).makedirs( File.dirname( expected_filename ) )
         mock(File).write( expected_filename, "bbb" )
-      
+
         subject.write_to_file( "aaa/bbb", "bbb" )
       end
     end
-    
+
     describe "when calling append_to_file()" do
       it "expands filename, opens file for append and appends content" do
         expected_filename = File.join( tmpdir, "aaa/bbb" )
         mock(FileUtils).makedirs( File.dirname( expected_filename ) )
         mock(File).open( expected_filename, "a" )
-      
+
         subject.append_to_file( "aaa/bbb", "bbb" )
       end
     end
   end
 
 end
-
-
